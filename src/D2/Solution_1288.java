1670a843a3442843fb1022a631531bef8d2eb9e2
WannyWanny <lejent3@naver.com>
2020-01-22 18:09:49 +0900

    D2

--- src/D2/Solution_1288.java
+++ src/D2/Solution_1288.java
@@ -1,14 +1,16 @@
 package D2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution_1288 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-
+        ArrayList<Integer> number = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
         for(int test_case=1; test_case<=T; test_case++){
-            
+
         }
     }
 }
