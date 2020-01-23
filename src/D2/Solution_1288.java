212ebaa9bdd73030caa22c060e8b7bdd2fd6cf05
WannyWanny <lejent3@naver.com>
2020-01-23 15:54:57 +0900

    D2

--- src/D2/Solution_1288.java
+++ src/D2/Solution_1288.java
@@ -1,16 +1,29 @@
 package D2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Solution_1288 {
-    public static void main(String[] args) {
+    public static void main(String args[]) throws Exception
+    {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        ArrayList<Integer> number = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for(int test_case=1; test_case<=T; test_case++){
-
+        int T;
+        T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int N = sc.nextInt();
+            int i=0;
+            Set<Integer> set = new HashSet<>();
+            while(set.size()!=10) {
+                int num = N*++i;
+                while(num!=0) {
+                    set.add(num%10);
+                    num/=10;
+                }
+            }
+            System.out.println("#"+test_case+" "+N*i);
         }
     }
 }
+
