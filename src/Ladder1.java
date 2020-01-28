e484840abd5339b9ed458c1bd18e044a11ed410e
WannyWanny <lejent3@naver.com>
2020-01-28 17:40:14 +0900

    1210

--- src/Ladder1.java
+++ src/Ladder1.java
@@ -1,45 +1,40 @@
 import java.util.Scanner;
 
 public class Ladder1 {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-	
-		for(int test_case = 1; test_case <= T; test_case++) {
-			int[][] input = new int[100][100];
-			int a = sc.nextInt();
-			int x=0;						//출구의 위치를 받을 좌표값
-			
-			//초기 배열값 할당
-			for(int i=0; i<100; i++)					
-			{
-				for(int j=0; j<100; j++) 
-				{
-					input[i][j] = sc.nextInt();
+		int[][] ladder = new int[102][102];
+		int y=0, x=0;
+
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			sc.nextInt();
+			for (int i = 1; i <= 100; i++) {
+				for (int j = 1; j <= 100; j++) {
+					ladder[i][j] = sc.nextInt();
+					if (ladder[i][j] == 2) { y = i; x = j; }
 				}
 			}
-			
-			//출구의 위치를 찾아보자. y좌표는 99이다.
-			for(int i=0; i<100; i++)
-			{
-					if(input[i][99] == 2)
-					{
-						x = i; 
+
+			while (y > 0) {
+				if (ladder[y][x+1] == 1) {
+					for (; x < 101; x++) {
+						if (ladder[y][x+1] == 0) { y--; break; }
 					}
+				}
+				else if (ladder[y][x-1] == 1) {
+					for (; x >= 0; x--) {
+						if (ladder[y][x-1] == 0) { y--; break; }
+					}
+				}
+				else {
+					for (; y >= 0; y--) {
+						if (ladder[y][x+1] == 1) break;
+						if (ladder[y][x-1] == 1) break;
+					}
+				}
 			}
-			
-			
-			
-		}
 
-	}
-	static int goUp(int[][] arr, int x, int y) {
-		if(arr[x][y-1] == 1)
-		{
-			goUp(arr, x, y-1);
+			System.out.println("#"+test_case+" "+(x-1));
 		}
-		return y;
 	}
-
 }
