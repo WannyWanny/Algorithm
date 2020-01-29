0f9539e60620042ce3858a7fb303b4dcb723e859
WannyWanny <lejent3@naver.com>
2020-01-29 18:02:51 +0900

    D4

---
+++ src/D4/Solution_1223.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int tc = 1; tc <= 10; tc++) {
+            Stack<Character> stack = new Stack<>();                     //연산자 담는 스택
+            int len = sc.nextInt();
+            String str = sc.next();
+            char[] ch = new char[len];
+            int ans=0;
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (Character.isDigit(c)) {
+                     ch[i]=stack.push(c);
+                    System.out.print(ch[i]);
+                }
+                else{
+                    if(stack.isEmpty()){
+                        stack.push(c);
+                    }
+                    else if(c == '+'){
+                        while(!stack.isEmpty()){
+                         ch[i]=stack.pop();
+                        }
+                        stack.push(c);
+                    }
+                    else if(c == '*'){
+                        while(!stack.isEmpty() && stack.peek() != '+'){
+                            ch[i]=stack.pop();
+                        }
+                         stack.push(c);
+                    }
+                }
+            }
+            for(int i=0; i<ch.length; i++) {
+                System.out.print(ch[i]);
+            }
+           // System.out.println("#"+tc+" "+ans);
+        }
+    }
+}
