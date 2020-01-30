c9386529b0b63dad07230a1d6ebe64a1805797f2
WannyWanny <lejent3@naver.com>
2020-01-30 17:57:50 +0900

    D2

--- src/D4/Solution_1223.java
+++ src/D4/Solution_1223.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Solution {
+public class Solution_1223 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         for (int tc = 1; tc <= 10; tc++) {
