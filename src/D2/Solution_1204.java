d69c835029da5aee28c17e46100899f435c13c41
이완희 <lejent3@naver.com>
2020-02-06 17:08:00 +0900

    D2

---
+++ src/D2/Solution_1204.java
@@ -0,0 +1,29 @@
+package D2;
+
+import java.util.Scanner;
+
+public class Solution_1204 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int tc=1; tc<=T; tc++){
+            sc.nextInt();
+            int[] score = new int[1001];
+            int[] cnt= new int[101];
+
+            for(int i=0; i<1000; i++){
+                score[i] = sc.nextInt();
+                cnt[score[i]]++;
+            }
+
+            int max = 0;
+
+            for(int i=0; i<100; i++){
+                if(score[max] <= score[i])
+                    max = i;
+            }
+
+            System.out.println("#"+tc+" "+max);
+        }//end of test
+    }
+}
