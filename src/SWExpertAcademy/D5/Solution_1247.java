87d0dc67b9772d100766ced37e1b3f453f400810
이완희 <lejent3@naver.com>
2020-02-19 00:19:53 +0900

    D5

--- src/SWExpertAcademy/D5/Solution_1247.java
+++ src/SWExpertAcademy/D5/Solution_1247.java
@@ -3,74 +3,59 @@ package SWExpertAcademy.D5;
 import java.util.Scanner;
 
 public class Solution_1247 {
-    static Point[] customers;
-    static Point home;
-    static Point company;
-    static int Ans = Integer.MAX_VALUE;;
+    static int T, N;
     static class Point{
-        int r, c;
+        int r;
+        int c;
         Point(int r, int c){
             this.r = r;
             this.c = c;
         }
     }
-    static int T, N;
+    static Point company;
+    static Point home;
+    static Point[] customers;
+    static int[] res;
+    static boolean[] visited;
+    static int Ans;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         T = sc.nextInt();
         for(int tc=1; tc<=T; tc++){
             N = sc.nextInt();
-            company =new Point(sc.nextInt(), sc.nextInt());
-            home = new Point(sc.nextInt(), sc.nextInt());
             customers = new Point[N];
+            res = new int[N];
+            visited = new boolean[N];
+
+            company = new Point(sc.nextInt(), sc.nextInt());
+            home = new Point(sc.nextInt(), sc.nextInt());
             for(int i=0; i<N; i++){
                 customers[i] = new Point(sc.nextInt(), sc.nextInt());
             }
-            sel = new int[N];
-            visited = new boolean[N];
-
-            perm(0);
+            Ans=Integer.MAX_VALUE;
+            Perm(0);
             System.out.println("#"+tc+" "+Ans);
         }
     }
 
-    static int[] sel;
-    static boolean[] visited;
-
-    static void backtrack(int idx, Point lastPoint, int dist){
-        if(idx == N){
-            dist+=(Math.abs(lastPoint.r - home.r) + Math.abs(lastPoint.c-home.c));
-            Ans = Math.min(Ans, dist);
-            return;
-        }
-
-        for(int i=0; i<N; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                backtrack(idx++, customers[i], dist+(Math.abs(lastPoint.r - customers[sel[i]].r)+Math.abs(lastPoint.c - customers[sel[i]].c)));
-                visited[i] = false;
-            }
-        }
-    }
-    static void perm(int idx){
-        if(idx == sel.length){
-            //정해진 순서대로 다 방문하기
-            Point lastPoint = company;
+    private static void Perm(int idx){
+        if(idx == res.length){
             int dist=0;
+            Point lasPoint=company;
             for(int i=0; i<N; i++){
-                dist += (Math.abs(lastPoint.r - customers[sel[i]].r)+Math.abs(lastPoint.c - customers[sel[i]].c));
-                lastPoint = customers[sel[i]];
+                dist += (Math.abs(lasPoint.r - customers[res[i]].r)+(Math.abs(lasPoint.c - customers[res[i]].c)));
+                lasPoint = customers[res[i]];
             }
-            dist += (Math.abs(lastPoint.r - home.r)+Math.abs(lastPoint.c - home.c));
+            dist += (Math.abs(lasPoint.r - home.r)+(Math.abs(lasPoint.c-home.c)));
             Ans = Math.min(Ans, dist);
             return;
         }
 
-        for(int i=0; i<N; i++){
+        for(int i=0; i<res.length; i++){
             if(!visited[i]){
-                sel[idx]=i;
+                res[idx]=i;
                 visited[i] = true;
-                perm(idx+1);
+                Perm(idx+1);
                 visited[i] = false;
             }
         }
