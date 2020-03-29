65a0ae99eb79c720ad5f8d1a026dc5a50b7f27e8
이완희 <lejent3@naver.com>
2020-03-29 22:37:39 +0900

    이분탐색

--- src/Baekjoon/이분탐색/Main_10816.java
+++ src/Baekjoon/이분탐색/Main_10816.java
@@ -7,5 +7,7 @@ public class Main_10816 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        int[] arr = new int[n];
     }
 }
