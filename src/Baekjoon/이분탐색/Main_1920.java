2e27fea5c43e0a0e8856a3720e5c54b44bc0168c
이완희 <lejent3@naver.com>
2020-03-30 13:23:29 +0900

    binarySearch

--- src/Baekjoon/이분탐색/Main_1920.java
+++ src/Baekjoon/이분탐색/Main_1920.java
@@ -1,24 +1,41 @@
 package Baekjoon.이분탐색;
 
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main_1920 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        HashSet<Integer> hs = new HashSet<Integer>();
-        for(int i=0; i<n; i++) hs.add(sc.nextInt());
+
+        int[] arr = new int[n];
+        for(int i=0; i<arr.length; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
 
         int m = sc.nextInt();
-        int[] arr = new int[m];
-        for(int i=0; i<m; i++) arr[i] = sc.nextInt();
-
         for(int i=0; i<m; i++){
-            if(hs.contains(arr[i]))
-                System.out.println("1");
-            else
-                System.out.println("0");
+            int num = sc.nextInt();
+            System.out.println(binarySearch(arr, num));
         }
+    }
+
+    private static int binarySearch(int[] A, int n){
+        int first = 0;
+        int last = A.length-1;
+        int mid = 0;
+
+        while(first<=last){
+            mid = (first+last)/2;
+
+            if(n == A[mid])
+                return 1;
+            else{
+                if(n < A[mid])
+                    last = mid-1;
+                else
+                    first = mid+1;
+            }
+        }
+        return 0;
     }
 }
