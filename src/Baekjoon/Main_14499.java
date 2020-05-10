248592f93458d32033f2ba3c7989e8e501bb5d93
이완희 <lejent3@naver.com>
2020-05-10 23:35:52 +0900

    SW

--- src/Baekjoon/Main_14499.java
+++ src/Baekjoon/Main_14499.java
@@ -1,7 +1,44 @@
 package Baekjoon;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main_14499 {
-    public static void main(String[] args) {
+    static class Dice{
+        int r;
+        int c;
+
+        public Dice(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+    static int R, C, K;
+    static int[][] map;
+    static int[] dir;
+    static int[] dr={0, 0, -1, 1};                 //동 서 남 북
+    static int[] dc={1, -1, 0, 0};
+    static ArrayList<Dice> list = new ArrayList<>();
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        R = sc.nextInt();
+        C = sc.nextInt();
+        int y = sc.nextInt();
+        int x = sc.nextInt();
+        K = sc.nextInt();
+        map = new int[R][C];
+        dir = new int[K];
+        list.add(new Dice(y, x));
+
+        for(int r=0; r<R; r++){
+            for(int c=0; c<C; c++){
+                map[r][c] = sc.nextInt();
+            }
+        }
+        for(int k=0; k<K; k++){
+            dir[k] = sc.nextInt()-1;
+        }
+
 
     }
 }
