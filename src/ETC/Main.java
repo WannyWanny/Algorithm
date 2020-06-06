263d358415b1585b48cf144b038e99905da8d8f6
이완희 <lejent3@naver.com>
2020-06-06 18:59:17 +0900

    SW

---
+++ src/ETC/Main.java
@@ -0,0 +1,12 @@
+package ETC;
+
+public class Main {
+    public static void main(String[] args) {
+        double n = 5000.0;
+        int cnt=0;
+        while(cnt<3){
+            cnt++;
+            
+        }
+    }
+}
