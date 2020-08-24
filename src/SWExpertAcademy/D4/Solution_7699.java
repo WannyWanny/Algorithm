7e7d434535f3c8cd02995abaf39fc1fdfb6927e0
wanny <lejent3@naver.com>
2020-08-24 13:55:32 +0900

    SW

--- src/SWExpertAcademy/D4/Solution_7699.java
+++ src/SWExpertAcademy/D4/Solution_7699.java
@@ -1,52 +1,51 @@
 package SWExpertAcademy.D4;
+
 import java.util.Scanner;
 
 public class Solution_7699 {
-    static int[][] map;
-    static int T, R, C;
-    static int[] dr={1, -1, 0, 0};         //상하좌우
-    static int[] dc={0, 0, -1, 1};
-    static int Ans;
-    static int[] visited;
-
+    static int R, C, ans;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int[][] arr;
+    static boolean[] alpha;
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        T = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
         for(int tc=1; tc<=T; tc++){
             R = sc.nextInt();
             C = sc.nextInt();
-            map = new int[R][C];
-            Ans=0;
-            visited = new int[26+1];
-            for(int r=0; r<R; r++){
+            arr = new int[R][C];
+            alpha = new boolean[26];
+            for(int r=0; r<R; r++) {
                 String str = sc.next();
-                for (int c=0; c<C; c++){
-                    map[r][c] = str.charAt(c)-'A';
+                for (int c = 0; c < C; c++) {
+                    arr[r][c] = str.charAt(c)-'A';
                 }
             }
-
-            DFS(0, 0, 1);
-            System.out.println("#"+tc+" "+Ans);
-
+            ans=0;
+            Search(0, 0, 1);
+            System.out.println("#"+tc+" "+ans);
         }
     }
 
-    private static void DFS(int r, int c, int cnt){
+    private static void Search(int r, int c, int cnt){
+//        값 비교
+        ans = Math.max(ans, cnt);
+//        초기 설정
+        alpha[arr[r][c]] = true;
 
-        Ans = Math.max(Ans, cnt);
-        visited[map[r][c]] = 1;
-        int nc, nr;
-
-        for(int k=0; k<4; k++){
-            nc = c+dc[k];
-            nr = r+dr[k];
-
-            if(nr<0 || nr>=R || nc<0 || nc>=C) continue;
-            if(visited[map[nr][nc]]==1) continue;
-
-            DFS(nr, nc, cnt+1);
-            visited[map[nr][nc]]=0;
-
+//        완전탐색
+        for(int d=0; d<4; d++){
+            int nc = c+dc[d];
+            int nr = r+dr[d];
+            if(isIn(nr, nc) && !alpha[arr[nr][nc]]){
+                Search(nr, nc, cnt+1);
+                alpha[arr[nr][nc]]=false;
+            }
         }
+    }
+
+    private static boolean isIn(int r, int c){
+        return (r>=0 && r<R && c>=0 && c<C);
     }
 }
