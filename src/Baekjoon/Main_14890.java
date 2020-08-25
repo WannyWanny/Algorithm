7ed06a10398aaa298601cd758b7dd49a138b5373
wanny <lejent3@naver.com>
2020-08-25 18:07:27 +0900

    SW

--- src/Baekjoon/Main_14890.java
+++ src/Baekjoon/Main_14890.java
@@ -3,8 +3,8 @@ package Baekjoon;
 import java.util.Scanner;
 
 public class Main_14890 {
-    static int[][] map;
-    static int N, L;
+    static int[][] map, map2;
+    static int N, L, ans;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
@@ -12,8 +12,21 @@ public class Main_14890 {
         map = new int[N][N];
         for(int r=0; r<N; r++){
             for(int c=0; c<N; c++){
-                map[r][c] = sc.nextInt();
+                map[r][c] = map2[c][r] = sc.nextInt();
             }
         }
+        ans=0;
+        for(int i=0; i<N; i++){
+            if(Solve(map[i])) ans++;
+            if(Solve(map2[i])) ans++;
+        }
+
+        System.out.println(ans);
+    }
+
+    private static boolean Solve(int[] arr){
+        int idx=0;
+        boolean[] visited = new boolean[N];
+        return true;
     }
 }
---
+++ src/Baekjoon/Main_2504.java
@@ -0,0 +1,10 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Main_2504 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+    }
+}
