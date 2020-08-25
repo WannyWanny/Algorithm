9590a85ee393bdf6979f26457301aa8a49121cf4
wanny <lejent3@naver.com>
2020-08-25 18:45:02 +0900

    SW

--- src/Baekjoon/Main_14890.java
+++ src/Baekjoon/Main_14890.java
@@ -10,6 +10,7 @@ public class Main_14890 {
         N = sc.nextInt();
         L = sc.nextInt();
         map = new int[N][N];
+        map2 = new int[N][N];
         for(int r=0; r<N; r++){
             for(int c=0; c<N; c++){
                 map[r][c] = map2[c][r] = sc.nextInt();
@@ -27,6 +28,39 @@ public class Main_14890 {
     private static boolean Solve(int[] arr){
         int idx=0;
         boolean[] visited = new boolean[N];
+
+        while(true){
+            if(idx == N-1) break;
+
+//            case 1: 내려가는 경사로
+            if(arr[idx+1] == arr[idx]-1){
+                if(idx+L >= N) return false;
+
+                for(int i=idx+1; i<=idx+L; i++){
+                    visited[i] = true;
+                    if(arr[i] != arr[idx]-1)
+                        return false;
+                }
+                idx += L;
+            }
+//            case 2: 올라가는 경사로
+            else if(arr[idx+1] == arr[idx]+1){
+                if(idx - (L-1) < 0) return false;
+
+                for(int i=idx-(L-1); i<=idx; i++){
+                    if(visited[i] || arr[i] != arr[idx])
+                        return false;
+                }
+                idx++;
+            }
+//            case 3: 평지
+            else if(arr[idx+1] == arr[idx]){
+                idx++;
+            }
+//            case 4: 높이차이가 L보다 큰 경우
+            else
+                return false;
+        }
         return true;
     }
 }
