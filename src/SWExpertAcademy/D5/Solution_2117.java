143cda7d200ec84aa8746acb506deb3eeafe01ad
이완희 <lejent3@naver.com>
2020-09-10 21:40:33 +0900

    SW

--- src/SWExpertAcademy/D5/Solution_2117.java
+++ src/SWExpertAcademy/D5/Solution_2117.java
@@ -1,7 +1,10 @@
 package SWExpertAcademy.D5;
 
+import java.util.Scanner;
+
 public class Solution_2117 {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
 
     }
 }
