da8ce1711c506c8d37e7dda5440fe3c0e31cc43b
이완희 <lejent3@naver.com>
2020-09-12 10:36:14 +0900

    SW

--- src/SWExpertAcademy/D5/Solution_2117.java
+++ src/SWExpertAcademy/D5/Solution_2117.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Solution_2117 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int tc=1; tc<=T; tc++){
 
+        }
     }
 }
