70c169ed5635d4fda835a26fb3c3b9cc1b116d79
이완희 <lejent3@naver.com>
2020-09-15 20:13:50 +0900

    SW

--- src/Baekjoon/Main_1939.java
+++ src/Baekjoon/Main_1939.java
@@ -1,7 +1,13 @@
 package Baekjoon;
 
+import java.util.Scanner;
+
 public class Main_1939 {
+    static int N,M;
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
 
     }
 }
